592fa58b89dba9f9040436ff23b8f110996cd0dd
Appu Rajendran <appu.rajendran@advance-auto.com>
2024-11-25 00:57:13 +0530

    sign/signup UI fix

--- pages/login/index.js
+++ pages/login/index.js
@@ -16,7 +16,8 @@ export default function LoginPage() {
   const router = useRouter();
   return (
     <>
-      <div className="md:hidden">
+      {/* Hide logo for mobile screens */}
+      <div className="hidden md:block">
         <Image
           src="/trans_logo.png?height=300&width=300"
           width={1280}
@@ -32,23 +33,26 @@ export default function LoginPage() {
           className="hidden dark:block"
         />
       </div>
-      <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+
+      {/* Main layout for Login page */}
+      <div className="container relative flex flex-col items-center justify-center py-8 md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <Link
           href="/signup"
-          className={cn(buttonVariants({ variant: 'ghost' }), 'absolute right-4 top-4 md:right-8 md:top-8')}
+          className={cn(buttonVariants({ variant: 'ghost' }), 'absolute right-4 top-4 md:right-8 md:top-8 z-10')}
         >
           Sign Up
         </Link>
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-primary" />
           <div className="relative z-20 flex items-center text-lg font-medium">
+            {/* Logo will now be hidden on mobile */}
             <Image
               onClick={() => router.push('/')}
               src="/trans_logo.png?height=40&width=40"
               width={40}
               height={40}
               alt="MakeMoneyMap Logo"
-              className="mr-2"
+              className="mr-2 cursor-pointer hidden md:block" // Hide on mobile (md and below)
             />
             MakeMoneyMap
           </div>
@@ -62,7 +66,7 @@ export default function LoginPage() {
             </blockquote>
           </div>
         </div>
-        <div className="lg:p-8">
+        <div className="lg:p-8 px-6">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">Welcome back</h1>
